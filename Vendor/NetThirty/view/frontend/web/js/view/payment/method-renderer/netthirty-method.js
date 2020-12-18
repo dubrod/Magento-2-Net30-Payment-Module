e6287398c9e32fafcea678dffcc52ecf6c162843
Wayne Roddy <dubrod@icloud.com>
2020-12-18 12:52:05 -0600

    Create netthirty-method.js

---
+++ Vendor/NetThirty/view/frontend/web/js/view/payment/method-renderer/netthirty-method.js
@@ -0,0 +1,20 @@
+define([
+    'Magento_Checkout/js/view/payment/default'
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Vendor_NetThirty/payment/netthirty'
+        },
+
+        /**
+         * Returns payment method instructions.
+         *
+         * @return {*}
+         */
+        getInstructions: function () {
+            return window.checkoutConfig.payment.instructions[this.item.method];
+        }
+    });
+});
